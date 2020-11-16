219644f596ac29a9e95befb630813d62408f8032
VartanBabayan <babayan.vartan@student.rau.am>
2020-11-16 17:15:41 +0400

    files for project

---
+++ BookSystemManager/BookSystemStruct.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cassert>
+#include <fstream>
+#include <exception>
+#include "nlohmann/json.hpp"
+using json = nlohmann::json;
+
+//Object Book
+class BookInfo {
+private:
+	std::string bookName{};
+	std::string authorName{};
+	unsigned int copiesQuantity{};
+
+public:
+	BookInfo() {}
+
+	BookInfo(std::string bookName, std::string authorName, int quantity)
+		: bookName(bookName),
+		authorName(authorName),
+		copiesQuantity(quantity)
+	{
+	}
+
+	friend void to_json(json& j, const BookInfo& p);
+	friend void from_json(const json& j, BookInfo& p);
+
+	~BookInfo() {}
+};
+
+//class that simulate all my BookSystem
+class BookSystem {
+private:
+	json j = json::array();
+public:
+	void AddBookRecords(const json& book);
+	void ShowBookRecords();
+	bool CheckAvailability(const json& book);
+	void ModifyBookRecords();
+	void DeleteBookRecords(const json& book);
+	json InputedInformation(); //book parsed into json
+
+
+	void readJSON();
+	void writeJson();
+
+	~BookSystem() {}
+};
