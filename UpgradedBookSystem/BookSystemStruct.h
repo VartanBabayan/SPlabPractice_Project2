a30e9763c489a9b7031624439cec4ccf9f654e6e
VartanBabayan <babayan.vartan@student.rau.am>
2020-11-19 22:57:49 +0400

    fixed bags: empty Base, input to FindBook, style

---
+++ UpgradedBookSystem/BookSystemStruct.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cassert>
+#include <fstream>
+#include <exception>
+#include "nlohmann/json.hpp"
+using json = nlohmann::json;
+
+// Object Book
+class BookInfo {
+private:
+	std::string bookName{};
+	std::string authorName{};
+	unsigned int copiesQuantity{};
+
+public:
+	BookInfo() {}
+
+	BookInfo(std::string bookName, std::string authorName, int quantity)
+		: bookName(bookName),
+		authorName(authorName),
+		copiesQuantity(quantity)
+	{
+	}
+
+	friend void to_json(json& j, const BookInfo& p);
+	friend void from_json(const json& j, BookInfo& p);
+
+	~BookInfo() {}
+};
+
+//class that simulate all my BookSystem
+class BookSystem {
+private:
+	json BookStorage = json::array();
+
+public:
+	void AddBookRecords(const json& book);
+	void ShowBookRecords() const;
+	bool CheckAvailability(const json& book) const;
+	void ModifyBookRecords();
+	void DeleteBookRecords(const json& book);
+
+	json InputedInformation() const; //book parsed into json
+	json inputToFindBook() const;
+
+	json getBase() const
+	{
+		return BookStorage;
+	}
+
+	void readJSON();
+	void writeJson();
+
+	~BookSystem() {}
+};
