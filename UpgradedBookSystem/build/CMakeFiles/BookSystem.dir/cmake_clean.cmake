9e2f14f499f11ccea835029ed9cc4d11798bfedd
VartanBabayan <babayan.vartan@student.rau.am>
2020-11-19 23:45:10 +0400

    fixed bugs: wrong input, empty array, style

---
+++ UpgradedBookSystem/build/CMakeFiles/BookSystem.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "BookSystem"
+  "BookSystem.pdb"
+  "CMakeFiles/BookSystem.dir/BookSystemStruct.cpp.o"
+  "CMakeFiles/BookSystem.dir/MainMenu.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BookSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
